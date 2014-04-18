5769b362d879fa540c93e67b6b2475d801df910f
Lifang <lifang.moler@gmail.com>
2014-04-18 08:55:26 +0000

    testing

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,7 +1,7 @@
 node 'precise64'{
     include puppet
     file { '/tmp/hello':
-           content => "Hello  kidding World\n",
-     }
+      content => "Hello World 3\n",
+    }
 }
 
--- modules/puppet/manifests/init.pp
+++ modules/puppet/manifests/init.pp
@@ -1,27 +1,28 @@
-class puppet {
+#To write a better puppet manifests
+class puppet{
 
-   file { '/usr/local/bin/papply':
-     source => 'puppet:///modules/puppet/papply.sh',
-     mode => '0755',
-   }
+  file { '/usr/local/bin/papply':
+    source => 'puppet:///modules/puppet/papply.sh',
+    mode   => '0755',
+  }
 
-   file { '/usr/local/bin/pull-updates':
-     source => 'puppet:///modules/puppet/pull-updates.sh',
-     mode => '0755'
-    }
-  
-   file { '/home/vagrant/.ssh/id_rsa':
-     source => 'puppet:///modules/puppet/ubuntu',
-     owner => 'vagrant',
-     mode => '0600',
-    }
-   
-   cron { 'run-puppet':
-      ensure => 'present',
-      user => 'vagrant',
-      command => '/usr/local/bin/pull-updates',
-      minute => '*/2',
-      hour => '*',
-     }
+  file { '/usr/local/bin/pull-updates':
+    source => 'puppet:///modules/puppet/pull-updates.sh',
+    mode   => '0755'
+  }
+
+#  file { '/home/vagrant/.ssh/id_rsa':
+#    source => 'puppet:///modules/puppet/ubuntu',
+#    owner  => 'vagrant',
+#    mode   => '0600',
+#  }
+
+  cron { 'run-puppet':
+    ensure  => 'present',
+    user    => 'vagrant',
+    command => '/usr/local/bin/pull-updates',
+    minute  => '*/2',
+    hour    => '*',
+  }
 
 }
